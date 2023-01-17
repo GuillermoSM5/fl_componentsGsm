fa4a3acacbc87f53b1138e4cfb0e8153e7891899
GuillermoSM <guillermosotomarquez5@gmail.com>
2023-01-16 18:18:46 -0600

    finish cardScreen

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -12,7 +12,23 @@ class CardScreen extends StatelessWidget {
         ),
         body: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          children: const [CustomCardType1()],
+          children: const [
+            CustomCardType1(),
+            SizedBox(height: 10),
+            CustomCardType2(
+              imageUrl:
+                  'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/02/fine-art-landscape-photography-1003.jpg?fit=1458%2C969&ssl=1',
+              textImg: 'Campo Bonito',
+            ),
+            SizedBox(height: 20),
+            CustomCardType2(
+                imageUrl:
+                    'https://images.squarespace-cdn.com/content/v1/5795a6914402433b8b4b3f74/1652319375667-SCDOQWQARKYR9Q16O6OA/IMG_4617.jpg'),
+            SizedBox(height: 20),
+            CustomCardType2(
+                imageUrl:
+                    'https://www.amateurphotographer.co.uk/wp-content/uploads/2022/03/Landscape-South-Island-New-Zealand-820x500.jpg')
+          ],
         ));
   }
 }
---
+++ lib/widgets/custom_card_type_2.dart
@@ -0,0 +1,36 @@
+import 'package:fl_components/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class CustomCardType2 extends StatelessWidget {
+  final String imageUrl;
+  final String? textImg;
+
+  const CustomCardType2({super.key, required this.imageUrl, this.textImg});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      clipBehavior: Clip.antiAlias,
+      elevation: 20,
+      shadowColor: AppTheme.primary.withOpacity(.5),
+      child: Column(
+        children: [
+          FadeInImage(
+            image: NetworkImage(imageUrl),
+            placeholder: const AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: const Duration(milliseconds: 300),
+          ),
+          if (textImg != null)
+            Container(
+                alignment: AlignmentDirectional.centerEnd,
+                padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+                child: Text(textImg!))
+        ],
+      ),
+    );
+  }
+}
